8afb7e18dff70c6e414019c546673e812c3fe00c
Sorokovsky <Sorokovskys@ukr.net>
2024-08-04 16:44:29 +0300

    refactor

--- src/constants/router.ts
+++ src/constants/router.ts
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import { lazy } from 'react';
 
 const HomePage = lazy(() => import("../components/pages/Home/Home"));
-const LotteryPage = lazy(() => import("../components/pages/Lottery/LotteryPage"));
+const LotteryPage = lazy(() => import("../components/pages/Lottery/Lottery"));
 export const router = createBrowserRouter([
     {
         path: '/',
