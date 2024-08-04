a0c2c0ea6326040024f75dca0821b1a1e6c45d5d
Sorokovsky <Sorokovskys@ukr.net>
2024-08-04 23:39:55 +0300

    edit

--- src/components/common/Row/Row.tsx
+++ src/components/common/Row/Row.tsx
@@ -1,9 +1,12 @@
 import { ChangeEvent, FC, memo, useCallback } from 'react';
 import Input from '../../ui/Input/Input';
 import { IRow } from './Row.interface';
+import { useLotteryStore } from '../../../store/lottery/lottery.store';
 
 const Row: FC<IRow> = ({ person }) => {
+    const editPerson = useLotteryStore(state => state.editPerson);
     const onChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
+        editPerson(person.id, event.target.value);
     }, []);
     return (
         <tbody>
