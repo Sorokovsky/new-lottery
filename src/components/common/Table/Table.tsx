8f62b499cd9b6d8fdffb018245ab8296bf18229a
Sorokovsky <Sorokovskys@ukr.net>
2024-08-05 15:18:23 +0300

    table

--- src/components/common/Table/Table.tsx
+++ src/components/common/Table/Table.tsx
@@ -1,20 +1,51 @@
-import { FC, memo } from 'react';
+import { FC, memo, useState, useCallback, useRef, useEffect } from 'react';
 import { ITable } from './Table.interface';
 import cn from "clsx";
 import styles from "./Table.module.sass";
 import Row from '../Row/Row';
 import AddPerson from '../AddPerson/AddPerson';
+import Button from '../../ui/Button/Button';
 
 const Table: FC<ITable> = ({ title, people }) => {
+    const [closed, setClosed] = useState(false);
+    const [result, setResult] = useState("Тут буде переможець");
+    const table = useRef<HTMLTableElement>(null);
+    const closeHandler = useCallback(() => {
+        setClosed(prev => !prev);
+    }, [closed]);
+
     return (
         <div className={cn(styles.wrapper)}>
-            <h3 className={cn(styles.heading)}>{title}</h3>
-            <table className={cn(styles.table)}>
+            <div className={cn(styles.head)}>
+                <h3 className={cn(styles.heading)}>
+                    {title}
+                </h3>
+                <button
+                    onClick={closeHandler}
+                    className={cn(styles.close, {
+                        [styles.closed]: closed
+                    })}
+                    type={'button'}
+                >
+                    +
+                </button>
+            </div>
+            <table ref={table} className={cn(styles.table, {
+                [styles.closed]: closed
+            })}>
                 {people.map(person => <Row key={person.id} person={person} />)}
+                <tbody>
+                    <tr>
+                        <td>
+                            <AddPerson />
+                        </td>
+                    </tr>
+                </tbody>
             </table>
-            <AddPerson />
+            <Button>Розіграти</Button>
+            <span className={cn(styles.result)}>{result}</span>
         </div>
-    )
-}
+    );
+};
 
 export default memo(Table);
